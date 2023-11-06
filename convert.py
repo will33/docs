e560a5bce269c83040a01208440a44eb7aeaa39f
William Fitzmaurice <william.fitzmaurice@nuonic.com.au>
2023-11-06 14:29:14 +1000

    Remove old code

--- convert.py
+++ convert.py
@@ -3,10 +3,5 @@ import json
 with open("openapi.json", "r") as f:
     data = json.load(f)
 
-data["components"]["securitySchemes"]["OAuth2PasswordBearer"] = {
-    "type": "http",
-    "scheme": "bearer",
-}
-
 with open("openapi.json", "w") as f:
     json.dump(data, f, indent=2)
